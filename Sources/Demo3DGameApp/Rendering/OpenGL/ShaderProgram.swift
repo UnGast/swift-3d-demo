e5728ce268a3b92be75ec72f7307bba36ddf52f0
UnGast <zadrian@web.de>
2020-09-21 22:17:06 +0200

    move shader logic into struct

--- Sources/Demo3DGameApp/Rendering/OpenGL/ShaderProgram.swift
+++ Sources/Demo3DGameApp/Rendering/OpenGL/ShaderProgram.swift
@@ -1,77 +1,160 @@
 import GL
 
-public struct ShaderError: Error {
-    let info: String
-
-    init(_ info: String) {
-        self.info = info
-    }
-}
-
 open class ShaderProgram {
+    
     public let vertexSource: String
+    
+    public let geometrySource: String?
+    
     public let fragmentSource: String
+
     public var id: GLMap.UInt = 0
 
-    public init(vertex vertexSource: String, fragment fragmentSource: String) {
+    public init(vertex vertexSource: String, geometry geometrySource: String? = nil, fragment fragmentSource: String) {
+
         self.vertexSource = vertexSource
+        
+        self.geometrySource = geometrySource
+
         self.fragmentSource = fragmentSource
     }
 
     deinit {
+
         glDeleteProgram(id)
     }
 
     /// compiles and links the shaders
     open func compile() throws {
-        let vertexShader = glCreateShader(GLMap.VERTEX_SHADER)
-        withUnsafePointer(to: vertexSource) { ptr in glShaderSource(vertexShader, 1, ptr, nil) }
-        glCompileShader(vertexShader)
 
-        let success = UnsafeMutablePointer<GLMap.Int>.allocate(capacity: 1)
-        let info = UnsafeMutablePointer<GLMap.Char>.allocate(capacity: 512)
-        glGetShaderiv(vertexShader, GLMap.COMPILE_STATUS, success)
-        if (success.pointee == 0) {
-            glGetShaderInfoLog(vertexShader, 512, nil, info)
-            throw ShaderError(String(cString: info))
-        } else {
-            print("Vertex shader successfully compiled.")
-        }
+        // TODO: maybe pass in the Shaders directly instead of the source
+        var vertexShader = Shader(type: .Vertex, source: vertexSource)
 
-        let fragmentShader = glCreateShader(GLMap.FRAGMENT_SHADER)
-        withUnsafePointer(to: fragmentSource) { ptr in GL.glShaderSource(fragmentShader, 1, ptr, nil) }
-        glCompileShader(fragmentShader)
-        glGetShaderiv(fragmentShader, GLMap.COMPILE_STATUS, success)
-        if (success.pointee == 0) {
-            glGetShaderInfoLog(fragmentShader, 512, nil, info)
-            throw ShaderError(String(cString: info))
-        } else {
-            print("Fragment shader successfully compiled.")
-        }
+        try vertexShader.compile()
+
+
+        /*var geometryShader: GLMap.UInt? = nil
+
+        if let geometrySource = geometrySource {
+            
+            geometryShader = glCreateShader(GLMap.GEOMETRY_SHADER)
+
+            withUnsafePointer(to: geometrySource) { glShaderSource(geometryShader!, 1, $0, nil) }
+
+            glCompileShader(geometryShader)
+
+
+        }*/
+
+
+        var fragmentShader = Shader(type: .Fragment, source: fragmentSource)
+
+        try fragmentShader.compile()
+
 
         self.id = glCreateProgram()
-        glAttachShader(self.id, vertexShader)
-        glAttachShader(self.id, fragmentShader)
+
+        glAttachShader(self.id, vertexShader.handle)
+
+        glAttachShader(self.id, fragmentShader.handle)
+
         glLinkProgram(self.id)
 
+        let success = UnsafeMutablePointer<GLMap.Int>.allocate(capacity: 1)
+
+        let info = UnsafeMutablePointer<GLMap.Char>.allocate(capacity: 512)
+
         glGetProgramiv(self.id, GLMap.LINK_STATUS, success)
+
         if (success.pointee == 0) {
+
             glGetProgramInfoLog(self.id, 512, nil, info)
-            throw ShaderError(String(cString: info))
-        } else {
-            print("Shader program linked successfully.")
+
+            throw LinkingError(description: String(cString: info))
         }
 
-        glDeleteShader(vertexShader)
-        glDeleteShader(fragmentShader)
+        glDeleteShader(vertexShader.handle)
+
+        glDeleteShader(fragmentShader.handle)
     }
 
     open func use() {
         
         if id == 0 {
+
             fatalError("Called use on shader before it was compiled.")
         }
 
         glUseProgram(id)
+    }
+}
+
+extension ShaderProgram {
+
+    public struct LinkingError: Error {
+
+        public var description: String
+    }
+}
+
+public struct Shader {
+
+    public var type: ShaderType
+
+    public var source: String
+
+    public var handle: GLMap.UInt = 0
+
+    mutating public func compile() throws {
+
+        let success = UnsafeMutablePointer<GLMap.Int>.allocate(capacity: 1)
+
+        let info = UnsafeMutablePointer<GLMap.Char>.allocate(capacity: 512)
+
+        handle = glCreateShader(type.glEnum)
+
+        withUnsafePointer(to: source) { ptr in GL.glShaderSource(handle, 1, ptr, nil) }
+
+        glCompileShader(handle)
+
+        glGetShaderiv(handle, GLMap.COMPILE_STATUS, success)
+
+        if (success.pointee == 0) {
+
+            glGetShaderInfoLog(handle, 512, nil, info)
+
+            throw CompilationError(description: String(cString: info))
+        }
+    }
+}
+
+extension Shader {
+
+    public enum ShaderType {
+
+        case Vertex, Geometry, Fragment
+
+        public var glEnum: Int32 {
+
+            switch self {
+
+            case .Vertex:
+
+                return GLMap.VERTEX_SHADER
+
+            case .Geometry:
+
+                return GLMap.GEOMETRY_SHADER
+
+            case .Fragment:
+
+                return GLMap.FRAGMENT_SHADER
+            }
+        }
+    }
+
+    public struct CompilationError: Error {
+
+        public var description: String
     }
 }
