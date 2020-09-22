875afd7ac8ed42f6aba71560ff060de59269aa3c
UnGast <zadrian@web.de>
2020-09-22 11:01:22 +0200

    add geometry shader to GLLineRenderer

--- Sources/Demo3DGameApp/Rendering/OpenGL/GLLineRenderer.swift
+++ Sources/Demo3DGameApp/Rendering/OpenGL/GLLineRenderer.swift
@@ -5,7 +5,11 @@ import CustomGraphicsMath
 public class GLLineRenderer {
 
     private var shaderProgram = ShaderProgram(
+
         vertex: vertexSource,
+        
+        geometry: geometrySource,
+        
         fragment: fragmentSource
     )
 
@@ -25,8 +29,13 @@ public class GLLineRenderer {
         glGenBuffers(1, &vbo)
         glBindBuffer(GLMap.ARRAY_BUFFER, vbo)
 
-        glVertexAttribPointer(0, 3, GLMap.FLOAT, false, GLMap.Size(MemoryLayout<Float>.size * 3), nil)
+        let stride = GLMap.Size(MemoryLayout<Float>.size * 7)
+
+        glVertexAttribPointer(0, 3, GLMap.FLOAT, false, stride, nil)
         glEnableVertexAttribArray(0)
+
+        glVertexAttribPointer(1, 4, GLMap.FLOAT, false, stride, UnsafeRawPointer(bitPattern: MemoryLayout<Float>.size * 3))
+        glEnableVertexAttribArray(1)
 
         glBindVertexArray(0)
         glBindBuffer(GLMap.ARRAY_BUFFER, 0)        
@@ -36,7 +45,20 @@ public class GLLineRenderer {
 
         glBindBuffer(GLMap.ARRAY_BUFFER, vbo)
         
-        glBufferData(GLMap.ARRAY_BUFFER, MemoryLayout<GLMap.Float>.size * 3 * lines.count * 2, lines.flatMap { $0.start.elements + $0.end.elements }.map(Float.init), GLMap.STATIC_DRAW)
+        var vertexData: [Float] = []
+
+        for line in lines {
+
+            vertexData.append(contentsOf: line.start.elements.map(Float.init))
+
+            vertexData.append(contentsOf: line.color.gl)
+
+            vertexData.append(contentsOf: line.end.elements.map(Float.init))
+
+            vertexData.append(contentsOf: line.color.gl)
+        }
+
+        glBufferData(GLMap.ARRAY_BUFFER, MemoryLayout<GLMap.Float>.size * vertexData.count, vertexData, GLMap.STATIC_DRAW)
 
         glBindBuffer(GLMap.ARRAY_BUFFER, 0)
 
@@ -66,22 +88,63 @@ extension GLLineRenderer {
 
     layout (location = 0) in vec3 positionIn;
 
+    layout (location = 1) in vec4 colorIn;
+
     uniform mat4 viewProjectionTransformation;
+
+    out VERTEX_OUT {
+        
+        vec4 color;
+
+    } vs_out;
 
     void main() {
 
         gl_Position = viewProjectionTransformation * vec4(positionIn, 1);
+
+        vs_out.color = colorIn;
+    }
+    """
+
+    private static let geometrySource = """
+    #version 330 core
+
+    layout (lines) in;
+
+    in VERTEX_OUT {
+
+        vec4 color;
+
+    } gs_in[];
+
+    layout (line_strip, max_vertices = 2) out;
+
+    out vec4 color;
+
+    void main() {
+
+        gl_Position = gl_in[0].gl_Position;
+        color = gs_in[0].color;
+        EmitVertex();
+
+        gl_Position = gl_in[1].gl_Position;
+        color = gs_in[1].color;
+        EmitVertex();
+
+        EndPrimitive();
     }
     """
 
     private static let fragmentSource = """
     #version 330 core
 
+    in vec4 color;
+
     out vec4 FragColor;
 
     void main() {
 
-        FragColor = vec4(1, 1, 1, 1);
+        FragColor = color;
     }
     """
 }
